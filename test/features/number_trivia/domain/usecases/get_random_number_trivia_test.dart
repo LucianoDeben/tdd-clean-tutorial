167175c4d73d9738f9ba41b78165e724480b72b0
LucidCodeAI <lutchocode@gmail.com>
2024-07-02 12:30:28 +0200

    Add TDD domain layer usecases

--- test/features/number_trivia/domain/usecases/get_random_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecases/get_random_number_trivia_test.dart
@@ -1,35 +1,36 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:myapp/core/usecases/usecase.dart';
 import 'package:myapp/features/number_trivia/domain/entities/number_trivia.dart';
-import 'package:myapp/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:myapp/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 
 import '../../../../mocks.mocks.dart';
 
 void main() {
-  late GetConcreteNumberTrivia usecase;
+  late GetRandomNumberTrivia usecase;
   late MockNumberTriviaRepository mockNumberTriviaRepository;
 
   setUp(() {
     mockNumberTriviaRepository = MockNumberTriviaRepository();
-    usecase = GetConcreteNumberTrivia(mockNumberTriviaRepository);
+    usecase = GetRandomNumberTrivia(mockNumberTriviaRepository);
   });
 
   const int tNumber = 1;
   const NumberTrivia tNumberTrivia =
       NumberTrivia(text: 'test', number: tNumber);
 
-  test('should get concrete number trivia from the repository', () async {
+  test('should get number trivia from the repository', () async {
     // Arrange
-    when(mockNumberTriviaRepository.getConcreteNumberTrivia(any))
+    when(mockNumberTriviaRepository.getRandomNumberTrivia())
         .thenAnswer((_) async => const Right(tNumberTrivia));
 
     // Act
-    final result = await usecase(const Params(number: tNumber));
+    final result = await usecase(NoParams());
 
     // Assert
     expect(result, const Right(tNumberTrivia));
-    verify(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber));
+    verify(mockNumberTriviaRepository.getRandomNumberTrivia());
     verifyNoMoreInteractions(mockNumberTriviaRepository);
   });
 }
